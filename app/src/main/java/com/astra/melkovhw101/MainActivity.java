5ffdde87ce1f4ed737966e99e6622103892d4d95
Melkov Denis <d.a.melkov@gmail.com>
2020-08-17 22:47:41 +0300

    remove toolbar (for correct swipelayout)

--- app/src/main/java/com/astra/melkovhw101/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw101/MainActivity.java
@@ -3,8 +3,10 @@ package com.astra.melkovhw101;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -20,61 +22,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        /* Toolbar */
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        String[] values = prepareContent();
-        //BaseAdapter listContentAdapter = createAdapter(values);
-        //List<Map<String, String>> content = new ArrayList<>();
-
-        List<Map<String, String>> content = createContent(values);
-        SimpleAdapter listContentAdapter = createAdapter(content);
-
+        /* ListView */
+        List<Map<String, String>> content = prepareContent();
         ListView list = findViewById(R.id.list);
-        list.setAdapter(listContentAdapter);
+        list.setAdapter(createAdapter(content));
+
+        /* SwipeRefreshLayout */
+        final SwipeRefreshLayout swipe = findViewById(R.id.main_swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                swipe.setRefreshing(false);
+            }
+        });
     }
 
-    private SimpleAdapter createAdapter(List<Map<String, String>> content) {
+    private BaseAdapter createAdapter(List<Map<String, String>> content) {
         String[] from = new String[]{"header","content"};
         int[] to = new int[]{R.id.header, R.id.content};
         return new SimpleAdapter(this, content, R.layout.list_item, from, to);
     }
 
-    private List<Map<String, String>> createContent(String[] values) {
+    @NonNull
+    private List<Map<String, String>> prepareContent() {
         List<Map<String, String>> result = new ArrayList<>();
 
+        String[] values = getString(R.string.large_text).split("\n\n");
         for(int i = 0; i < values.length; i++) {
             Map<String, String> item = new HashMap<>();
 
-            String headerValue = values[i];
+            String headerValue = values[i].trim();
             i++;
-            String contentValue = "";
+            StringBuilder contentValue = new StringBuilder();
             while(values[i].length() > 100) {
-                contentValue += values[i];
-                if(i < values.length - 1) {
-                    i++;
-                } else {
-                    i++;
+                contentValue.append(values[i].trim());
+
+                i++;
+                if(i >= values.length - 1) {
                     break;
                 }
             }
             i--;
 
             item.put("header", headerValue);
-            item.put("content", contentValue);
+            item.put("content", contentValue.toString());
             result.add(item);
         }
 
         return result;
-    }
-
-//    @NonNull
-//    private BaseAdapter createAdapter(String[] values) {
-//        return new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
-//    }
-
-    @NonNull
-    private String[] prepareContent() {
-        return getString(R.string.large_text).split("\n\n");
     }
 }
